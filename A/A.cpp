5112cbd934c1d867f500dec2161909da7eaf00bd
leoleoasd <luyuxuanleo@gmail.com>
2017-09-24 10:23:08 +0800

    WHOA

--- A/A.cpp
+++ A/A.cpp
@@ -9,8 +9,8 @@ struct road{
     int data;
 };
 
-struct road data[2020];
-
+struct road data[100020];
+ 
 int cmp(struct road a,struct road b){
     return a.data<b.data;
 }
@@ -27,15 +27,78 @@ void read(int &x){
         p=getchar();
     }
     while('0'<= p and p <= '9'){
-        x = x*10+p-'0';
+        x = ((x<<3) + (x<<1))+p-'0';
         p=getchar();
     }
     x*=f;
 }
+int n,m,q;
+int mmmmmmm = 0;
+short father[20020];
+short rank[20020];
+int find_set(int node){  
+    if (father[node] != node) {  
+        father[node] = find_set(father[node]);
+    }
+    return father[node];
+}
+int merge(int pa,int qa){
+    int root1 = find_set(pa);  
+    int root2 = find_set(qa);  
+    if(root1==root2){
+        return 0;
+    }
+    if(rank[root1] > rank[root2]){
+        father[root2] = root1;
+        rank[root1] += rank[root2];
+        rank[root2]=0;
+    }  
+    else{
+        father[root1] = root2;  
+        rank[root2] += rank[root1];  
+        rank[root1]=0;
+    }  
+    return 1;  
+}  
+void zt(){
+    read(n);
+    read(m);
+    read(q);
+    for(int i=0;i<m;i++){
+        read(data[i].from);
+        read(data[i].to);
+        read(data[i].data);
+    }
+    sort(data,data+m,cmp);
+    for(int i=0;i<q;i++){
+        for(int j = 1;j < n;++j){  
+            father[j] = j;  
+            rank[j] = 1;
+        }
+        read(mmmmmmm);
+        for(int j=0;j<m;j++){
+            if(data[j].data > mmmmmmm){
+                break;
+            }
+            merge(data[j].from-1,data[j].to-1);
+        } 
+        int output=0;
+        for(int j=0;j<n;j++){
+            output+=(rank[j]*(rank[j]-1));
+        }
+        cout<<output<<endl;
+    }
+}
 
 int main(){
+    freopen("A.in","r",stdin);
+    freopen("A.out","w",stdout);
     int test;
-    int a=1132;
-    cout<<(a<<3)+(a<<1);
+    read(test);
+    while(--test>=0){
+        zt();
+    }
+    fclose(stdin);
+    fclose(stdout);
     return 0;
 }
