6e0716a37321eb9c501b24f218d0e06bb663c278
leoleoasd <luyuxuanleo@gmail.com>
2017-10-06 08:02:43 +0000

    judged question124/4

--- 124/4.cpp
+++ 124/4.cpp
@@ -24,18 +24,10 @@ int data[100000]={0};
 int main(){
     int a,b,c;
     cin>>a;
-    if(a>=0){
-        if(a%2==0){
-            cout<<a*a;
-        }else{
-            cout<<a-10;
-        }
-    }else{
-        if(a%2==0){
-            cout<<1-a*a*a;
-        }else{
-            cout<<10-a;
-        }
+    b=1;
+    for(int i=a;i>=1;--i){
+        b*=i;
     }
+    cout<<b;
     return 0;
 }
