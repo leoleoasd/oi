c36e8b4175b6c9facab47fbee6670e706a01966e
leoleoasd <luyuxuanleo@gmail.com>
2017-10-08 03:19:43 +0000

    judged questionB/B

---
+++ B/B.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cmath>
+#include <map>
+using namespace std;
+
+ifstream fin("B.in");
+ofstream fout("B.out");
+
+#define fin cin
+#define fout cout
+#define debug 1
+
+#ifdef debug
+    #define LOG(A) cout<<A<<" ";
+#else
+    #define LOG(A)
+#endif
+#define MOD 998244353
+void solve(){
+    
+    int n,m,k;
+    fin>>n>>m>>k;
+    for(int i=0;i<n;i++) fin>>m;
+    int a=0;
+    while(k!=1) {
+        k<<1;
+        a++;
+    }
+    while(--a){
+        k>>1;
+    }
+    k>>1;
+    fout<<k;
+    
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    int n;
+    fin>>n;
+    while(n--){
+        solve();
+    }
+    return 0;
+}
