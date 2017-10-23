dd2e5126e387596c43cc4113d5e28a988658f898
leoleoasd <luyuxuanleo@gmail.com>
2017-10-23 07:49:12 +0000

    judged question124/p108

--- 124/p108.cpp
+++ 124/p108.cpp
@@ -9,58 +9,31 @@
 #include <cstdio>
 
 using namespace std;
-
-#define DEBUG 1
-#ifdef DEBUG
-    #define LOG(A) do{cout<<A<<endl;}while(0)
-#else
-    #define LOG(A)l
-#endif
-
-#define MAXN 2000
-#define min(A,B) (((A)>(B))?(A):(B))
-int num=0;
-struct node{
-    int id;
-    int first;
-    node(){
-        this->id=0;
-        this->first=0;
-    }
-    void add_road(int to,int value){
-        int fr = this->first;
-        bian[num].to=to;
-        bian[num].value=value;
-        bian[num].next=fr;
-        ++num;
-        this->first=num-1;
-    }
-} w[2000];
-struct bian{
-    int to;
-    int next;
-    int value;
-    bian(){
-        this->to=0;
-        this->next=0;
-        this->value=0;
-    }
-} list[20000];
-int n,m;
-int dd=0;
-int dfs(int now){
-    if(dd!=0){
-        
-    }
-}
-void bfs(){
+#define MAXN 2020
+int data[MAXN][MAXN]={0};
+int b = 0;
+int d = 0;
+void bfs(int pos){
     
 }
-void init(){
-    for(int i=1;i<=1999;i++){
-        node[i].id=i;
+void dfs(int pos){
+    if(visited[i]){
+        return;
+    }
+    visited[i]=1;
+    if(d){
+        
+    }else{
+        cout<<"-";
+    }
+    cout<<pos;
+    for(int i=1;i<=n;++i){
+        if(data[pos][i]==1 and visited[i]==0){
+            dfs(i);
+        }
     }
 }
+bool visited[MAXN]={0};
 
 int main(){
     init();
@@ -68,9 +41,16 @@ int main(){
     cin>>n>>m;
     while(--m){
         cin>>a>>b;
-        add_road(a,b,1);
+        data[a][b]=data[b][a]=1;
     }
-    bfs();
-    dfs();
+    for(int i=1;i<=n;++i){
+        if(!visited[i])
+            dfs(i);
+    }
+    for(int i=0;i<MAXN;++i)visited[i]=false;
+    for(int i=1;i<=n;++i){
+        if(!visited[i])
+            bfs(i);
+    }
     return 0;
 }
