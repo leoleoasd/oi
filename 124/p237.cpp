16595797c999c45bc17e7755c65e1e8387fe058f
leoleoasd <luyuxuanleo@gmail.com>
2017-10-24 01:38:19 +0000

    judged question./124/p237

--- 124/p237.cpp
+++ 124/p237.cpp
@@ -17,23 +17,4 @@ int main(){
     }
     cout<<fall();
     return 0;
-}#include <iostream>
-#include <cmath>
-using namespace std;
-bool tree[1100000]={false};
-int d,l;
-int fall(){
-    int now = 1;
-    while(now>>(d-1)<1){
-        cout<<now<<endl;
-        now = now*2+1;
-    }
-}
-int main(){
-    cin>>d>>l;
-    for(int i=1;i<l;++i){
-        fall();
-    }
-    cout<<fall();
-    return 0;
 }
