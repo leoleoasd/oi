4e69cbddeb686fba5e716f650dd2ac50fdc3dc71
leoleoasd <luyuxuanleo@gmail.com>
2017-10-24 01:08:42 +0000

    judged questionbisai/rect/rect

--- bisai/rect/rect.cpp
+++ bisai/rect/rect.cpp
@@ -3,6 +3,14 @@
 #include <stdlib.h>
 #include <time.h>
 using namespace std;
+
+ifstream fin("rect.in");
+ofstream fout("rect.out");
+
+#define cin fin
+#define cout fout
+
+
 int main(){
     srand((unsigned)time(NULL));
     cout<<rand()%100+1;
