81fd1abc6a4c8b0066e293dde47e473794d3a23b
leoleoasd <luyuxuanleo@gmail.com>
2017-10-24 02:33:17 +0000

    judged question./bisai/rect/rect

--- bisai/rect/rect.cpp
+++ bisai/rect/rect.cpp
@@ -4,19 +4,12 @@ using namespace std;
 
 ifstream fin("rect.in");
 ofstream fout("rect.out");
-
-//#define cin fin
-//#define cout fout
-long long ASD(int n){
-    long long a = 1;
-    for(int i=1;i<=n;++i){
-        a *= i;
-    }
-    return a;
+const long long jc[] = {1,1,2,6,24,120,720,5040,40320,362880,3628800,39916800,479001600,6227020800,87178291200,1307674368000,20922789888000,355687428096000,6402373705728000,121645100408832000,2432902008176640000};
+long long C(int b,int a){
+    return (jc[b])/((jb[a])*jc[b-a]);
 }
+
 int main(){
-    for(int i=0;i<=25;++i){
-        cout<<ASD(i)<<",";
-    }
+    cout<<C(3,1);
     return 0;
 }
