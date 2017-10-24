67885cb463a323614be1d48b0d5197cf57dc588d
leoleoasd <luyuxuanleo@gmail.com>
2017-10-24 03:25:45 +0000

    judged question./bisai/gcd/gcd

--- bisai/gcd/gcd.cpp
+++ bisai/gcd/gcd.cpp
@@ -1,11 +1,11 @@
 #include <cstdio>
 #include <iostream>
-#include <ccstdlib>
+#include <cstdlib>
 #include <ctime>
 int main(){
-    int n =0 ;
+    int n =0;
     cin>>n;
-    srand( (unsigned)time( NULL ) );
-　　for(int i = 0; i < n;i++ )
-　　　　printf("%d ", rand() );
+    srand((unsigned)time(NULL));
+　　for(int i=0;i<n;i++)
+　　　　cout<<rand()<<" ";
 }
