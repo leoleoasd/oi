0809fe7e5a0cc667d49b771e4497309328fc2fbd
leoleoasd <luyuxuanleo@gmail.com>
2017-10-24 02:44:36 +0000

    judged question./bisai/rect/rect

--- bisai/rect/rect.cpp
+++ bisai/rect/rect.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <algorithm>
 using namespace std;
 
 ifstream fin("rect.in");
@@ -8,8 +9,42 @@ const long long jc[] = {1,1,2,6,24,120,720,5040,40320,362880,3628800,39916800,47
 long long C(int b,int a){
     return (jc[b])/((jc[a])*jc[b-a]);
 }
-
+#define MAXN 2000
+#define LL long long
+LL bian[MAXN][4]={0};
+LL type[MAXN]={0};   // 1为竖直  0 水平
+int n;
 int main(){
-    cout<<C(8,2);
+    cin>>n;
+    for(int i=0;i<n;++i){
+        cin>>bian[i][0]>>bian[i][1]>>bian[i][2]>bian[i][3];
+        if(bian[i][0]==bian[i][2]){
+            type[i]=1;
+            if(bian[i][3]<bian[i][1]){
+                swap(bian[i][1],bian[i][3]);
+            }
+        }else{
+            if(bian[i][2] < bian[i][0]){
+                swap(bian[i][0],bian[i][2]);
+            }
+        }
+    }
+    int output = 0;
+    int mi,ma;
+    for(int i=0;i<n;++i){
+        if(type[i]==1){
+            for(int j=i+1;j<n;++j){
+                if(type[j]==1){
+                    mi = min(bian[i][1],bian[j][1]);
+                    ma = max(bian[i][3],bian[j][3]);
+                    for(int k=0;k<n;++k){
+                        if(type[k]==0){
+                            
+                        }
+                    }
+                }
+            }
+        }
+    }
     return 0;
 }
