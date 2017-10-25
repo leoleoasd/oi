2dba474ae3d6239394122a718b62a1ad2207fa7a
leoleoasd <luyuxuanleo@gmail.com>
2017-10-25 14:05:19 +0000

    judged questionlg/1042

--- lg/1042.cpp
+++ lg/1042.cpp
@@ -1,29 +1,32 @@
 #include <iostream>
 using namespace std;
+int abs(int i){
+    return i>0?i:-i;
+}
 int main(){
     char tmp;
-    int a11[200][2]={0};
+    int a11[3000][2]={0};
     int a11pos = 0;
-    int a21[200][2]={0};
+    int a21[3000][2]={0};
     int a21pos = 0;
     cin>>tmp;
     while(tmp != 'E'){
         if(tmp == 'W'){
             a11[a11pos][0]++;
-            if(a11[a11pos][0]+a11[a11pos][1]==11){
+            if((a11[a11pos][0]>=11 or a11[a11pos][1]>=11)and abs(a11[a11pos][0]-a11[a11pos][1])>=2 ){
                 ++a11pos;
             }
             a21[a21pos][0]++;
-            if(a21[a21pos][0]+a21[a21pos][1]==21){
+            if(a21[a21pos][0]>=11 or a21[a21pos][1]>=21){
                 ++a21pos;
             }
         }else{
             a11[a11pos][1]++;
-            if(a11[a11pos][0]+a11[a11pos][1]==11){
+            if(a11[a11pos][0]>=11 or a11[a11pos][1]>=11){
                 ++a11pos;
             }
             a21[a21pos][1]++;
-            if(a21[a21pos][0]+a21[a21pos][1]==21){
+            if(a21[a21pos][0]>=21 or a21[a21pos][1]>=21){
                 ++a21pos;
             }
         }
