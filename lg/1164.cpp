9bc3cf9168c9336fe35738c6fd2e9fbf3ee03215
leoleoasd <luyuxuanleo@gmail.com>
2017-10-25 12:10:22 +0000

    judged questionlg/1060

---
+++ lg/1164.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+    #define MAXN 100
+    int m,n;
+    int a[MAXN]={0};
+    int ways[MAXN]={0};
+    cin>>n>>m;
+    for(int i = 0;i<n;++i){
+        cin>>a[i];
+        ++ways[a[i]];
+    }
+    sort(a,a+n);
+    cout<<a[0];
+    return 0;
+    for(int i=0;i<=m;++i){
+        for(int j=0;j<n;++j);
+    }
+    return 0;
+}
