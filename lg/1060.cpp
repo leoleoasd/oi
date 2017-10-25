2b262c7e124589783a194042dd6b1c38815a17f1
leoleoasd <luyuxuanleo@gmail.com>
2017-10-25 11:51:01 +0000

    judged questionlg/1060

--- lg/1060.cpp
+++ lg/1060.cpp
@@ -2,12 +2,17 @@
 #include <fstream>
 #include <algorithm>
 using namespace std;
+#define MAXN 40
+int w[MAXN]={0};
+int r[MAXN]={0};
+int n,m;
+int dp[MAXN][30020]={0};
+void output(){
+    for(int i=0;i<m;++i){
+        for(int )
+    }
+}
 int main(){
-    #define MAXN 40
-    int w[MAXN]={0};
-    int r[MAXN]={0};
-    int n,m;
-    int dp[MAXN][30020]={0};
     cin>>n>>m;
     for(int i=0;i<m;++i){
         cin>>w[i]>>r[i];
@@ -15,7 +20,7 @@ int main(){
     for(int i=0;i<=n;++i){
         dp[0][i] = i > w[0] ? w[0]*r[0] : 0;
     }
-    for(int i=1;i<=m;++i){
+    for(int i=1;i<m;++i){
         for(int j=0;j<=n;++j){
             if(j-w[i]>=0){
                 dp[i][j]=max(dp[i][j-1],dp[i-1][j-w[i]]+w[i]*r[i]);
@@ -24,7 +29,9 @@ int main(){
                 dp[i][j]=dp[i][j-1];
             }
         }
+        output();
+        
     }
-    cout<<dp[m][n];
+    cout<<dp[m-1][n];
     return 0;
 }
