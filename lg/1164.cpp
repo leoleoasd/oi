cefa949da07f0f9867f0c9e089fb85af601c901e
leoleoasd <luyuxuanleo@gmail.com>
2017-10-25 12:20:36 +0000

    judged questionlg/1164

--- lg/1164.cpp
+++ lg/1164.cpp
@@ -7,13 +7,13 @@ int main(){
     int a[MAXN]={0};
     int ways[MAXN]={0};
     cin>>n>>m;
-    for(int i = 0;i<n;++i){
+    for(int i=0;i<n;++i){
         cin>>a[i];
         //++ways[a[i]];
     }
     ways[0]=1;
     sort(a,a+n);
-    for(int i=1;i<=n;i++)
+    for(int i=0;i<n;i++)
     for(int j=m;j>=a[i];j--)
     ways[j]+=ways[j-a[i]];
     cout<<ways[m];
