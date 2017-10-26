860fba4b648423d46da31392349f07f418066a0f
leoleoasd <luyuxuanleo@gmail.com>
2017-10-26 07:53:32 +0000

    judged question./lg/1223

---
+++ 124/p167.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define MAXN 10020
+int data[MAXN]={0};
+int n;
+int dp[MAXN]={0};
+void solve(){
+    int maxx = -1;
+    for(int i=1;i<=n;++i){
+        maxx = -1;
+        for(int j=1;j<=i;++j){
+            if(num[i]>num[j])
+            maxx = max(maxx,dp[j]);
+        }
+        dp[i] = dp[j]+1;
+    }
+}
+int main(){
+    cin>>n;
+    for(int i=1;i<=n;++i){
+        cin>>data[i];
+    }
+    solve();
+    int xxx = -1;
+    for(int i=1;i<=n;++i){
+        xxx = max(xxx,dp[i]);
+    }
+    cout<<xxx;
+    return 0;
+}
