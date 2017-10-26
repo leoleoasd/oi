a83340750792ba7bac38b0bd2762549a5a0043a5
leoleoasd <luyuxuanleo@gmail.com>
2017-10-26 05:15:19 +0000

    judged question./bisai/f/f

---
+++ lyx/g/g.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+ifstream fin("g.in");
+ofstream fout("g.out");
+#define cin fin
+#define cout fout
+int main(){
+    cout<<"2.589"<<endl<<"1.976";
+    return 0;
+}
---
+++ lyx/h/h.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+ifstream fin("h.in");
+ofstream fout("h.out");
+#define cin fin
+#define cout fout
+int main(){
+    cout<<"0.555556";
+    return 0;
+}
