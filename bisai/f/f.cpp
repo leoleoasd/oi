a9b7a95bd72c2fd6580e009fa9d0cfc21083a55a
leoleoasd <luyuxuanleo@gmail.com>
2017-10-26 00:33:39 +0000

    judged question./bisai/f/f

--- bisai/f/f.cpp
+++ bisai/f/f.cpp
@@ -5,11 +5,14 @@ using namespace std;
 #define MAXN 16
 int data[MAXN][MAXN]={0};
 bool vis[MAXN][MAXN]={false};
-int r,c;
+int r,c;,now,sum;
 struct node{
     int x;
     int y;
     int value;
+    node(){
+        x=y=value=0;
+    }
     node(int a,int b,int c){
         x=a;
         y=b;
@@ -18,11 +21,17 @@ struct node{
     bool operator<(node a){
         return value < a.value;
     }
+    bool operator>(node a){
+        return value > a.value;
+    }
 };
+node P[MAXN];
 
 int main(){
-    cout<<(node(1,2,3)<node(1,2,4));
-    //cin>>r>>c;
-    
+    cin>>r>>c;
+    while(r!=0 and c!=0){
+        cout<<123<<endl;
+        cin>>r>>c;
+    }
     return 0;
 }
