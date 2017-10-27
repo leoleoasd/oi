8c3b531a3491e2b9aafb56c9135eda212dd80ff8
leoleoasd <luyuxuanleo@gmail.com>
2017-10-27 03:18:36 +0000

    judged questionlyx/b/b

--- lyx/b/b.cpp
+++ lyx/b/b.cpp
@@ -2,7 +2,10 @@
 #include <cstdlib>
 #include <fstream>
 using namespace std;
-
+ifstream fin("b.in");
+ofstream fout("b.out");
+#define cin fin
+#define cout fout
 int main(){
     cout<<rand()%10;
     return 0;
