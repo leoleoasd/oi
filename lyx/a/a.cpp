461ce753da8cf3f6f522815def8d849c994d5bdc
leoleoasd <luyuxuanleo@gmail.com>
2017-10-27 01:34:18 +0000

    judged questionlyx/a/a

--- lyx/a/a.cpp
+++ lyx/a/a.cpp
@@ -19,7 +19,7 @@ int calc(int a,int l,int r){
 void solve(int x,int y){
     int ret = 0;
     for(int i=0;i<n-1;++i){
-        for(int j=0;j<n-i;++j){
+        for(int j=0;j<=n-i;++j){
             if(calc(x,i,i+j)==calc(y,i,i+j)){
                 ++ret;
             }
