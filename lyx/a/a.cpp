0775df427e2b95c17509cc07ad547c6ecce1db2a
leoleoasd <luyuxuanleo@gmail.com>
2017-10-27 01:32:15 +0000

    judged questionlyx/a/a

---
+++ lyx/a/a.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fsttream>
+#include <cstdlib>
+#include <vector>
+using namespace std;
+#define MAXN 8080
+int data[MAXN]={0};
+vector<int> ap[MAXN];
+int n,q;
+int calc(int a,int l,int r){
+    int ret = 0;
+    for(int i=0;i<data[a].size();++i){
+        if(l<=data[a][i] and data[a][i]<=r){
+            ++ret;
+        }
+    }
+    return ret;
+}
+void solve(int x,int y){
+    int ret = 0;
+    for(int i=0;i<n-1;++i){
+        for(int j=0;j<n-i;++j){
+            if(calc(x,i,i+j)==calc(y,i,i+j)){
+                ++ret;
+            }
+        }
+    }
+    cout<<ret<<endl;
+}
+int main(){
+    cin>>n>>q;
+    for(int i=0;i<n;++i){
+        cin>>data[i];
+        ap[data[i]].push_back(i);
+    }
+    int x,y;
+    for(int i=0;i<q;++i){
+        cin>>x>>y;
+        solve(x,y);
+    }
+    return 0;
+}
