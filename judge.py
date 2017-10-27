56957c22e51d16672195be2329da34983f2494c9
leoleoasd <luyuxuanleo@gmail.com>
2017-10-27 01:52:38 +0000

    judged questionlyx/a/a

--- judge.py
+++ judge.py
@@ -11,7 +11,7 @@ def main():
     system("g++ -g -o "+t+" "+t+".cpp"+byxx)
     print("Compile Endded")
     print("------------------------------------------------------------------------")
-    system("time "+t)
+    system("bash -c \"time "+t+"\"")
     print("")
     print("------------------------------------------------------------------------")
     print("git committing")
