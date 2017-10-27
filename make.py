47a4a59241fdedef8bca4334de17e8b175f71cc7
leoleoasd <luyuxuanleo@gmail.com>
2017-10-27 02:40:49 +0000

    judged questionlyx/a/a

--- make.py
+++ make.py
@@ -6,11 +6,15 @@ try:
     n = 100
     q = 1000
     f.write(str(n)+" "+str(q)+"\n")
-    for i in range(1,n):
-        f.write(str(random.randint(1,1000000))+" ")
+    a = []
+    for i in range(0,n):
+        tmp = random.randint(1,1000000)
+        a.append(tmp)
+        f.write(str(a[-1])+" ")
     f.write("\n")
-    for i in range(1,q):
-        f.write(str(random.randint(1,1000000))+" "+str(random.randint(1,1000000))+"\n")
+    print(len(a))
+    for i in range(0,q):
+        f.write(str(a[random.randint(0,98)])+" "+str(a[random.randint(0,98)])+"\n")
 finally:
     if f:
         f.close()
