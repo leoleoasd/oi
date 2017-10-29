08cfc3e003ba52a74a9667262ba9badef4c99c0c
os <os@osdeMacBook-Pro.local>
2017-10-29 10:40:57 +0800

    judged questionlyx/mai/mai

--- make.py
+++ make.py
@@ -2,19 +2,17 @@
 # -*- coding: UTF-8 -*-
 import random
 try:
-    f = open('a.in', 'w')
-    n = 200
-    q = 1000
-    f.write(str(n)+" "+str(q)+"\n")
-    a = []
-    for i in range(0,n):
-        tmp = random.randint(1,20)
-        a.append(tmp)
-        f.write(str(a[-1])+" ")
+    f = open('mai.in', 'w')
+    w=5000
+    n=5000
+    d=5000
+    f.write(str(w)+" "+str(n)+" "+str(d)+" ")
+    for i in range(0,d):
+        tmp = random.randint(1,1000000)
+        f.write(str(tmp)+" ")
     f.write("\n")
-    print(len(a))
-    for i in range(0,q):
-        f.write(str(a[random.randint(0,98)])+" "+str(a[random.randint(0,98)])+"\n")
+    for i in range(0,n):
+        f.write(str(random.randint(0,1000000))+" "+str(random.randint(0,1000000))+" "+str(random.randint(0,1000000))+" "+"\n")
 finally:
     if f:
         f.close()
