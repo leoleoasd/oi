7080f1d8f8a7495fc79cbf1a83db2f2095c73aa7
os <os@osdeMacBook-Pro.local>
2017-10-29 09:57:47 +0800

    judged questionlyx/mai/mai

---
+++ lyx/mai/mai.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+#define MAXN 5050
+int a[MAXN]={0};
+int b[MAXN]={0};
+int c[MAXN]={0};
+int d[MAXN]={0};
+int w;
+int n;
+int D;
+int dp[MAXN]={0};
+// 转移方程 f[k][v]=max{f[k-1][v],f[k-1][v-c[i]]+w[i]|物品i属于组k}
+#define max(A,B) ((A)>(B)?(A):(B))
+int main(){
+    ios::sync_with_stdio(false);
+    cin>>w>>n>>d;
+    for(int i=1;i<=n;++i){
+        cin>>d[i];
+    }
+    for(int i=1;i<=n;++i){
+        cin>>a[i]>>b[i]>>c[i];
+    }
+    for(int k=1;k<=d;++k){
+        for(int v = 0;v<w;++v){
+            for(int i=1;i<=n;++i){
+                if(c[i]==k and v > a[i]){
+                    dp[v]=max(dp[v],dp[v-a[i]]+b[i]*d[k])
+                }
+            }
+        }
+    }
+    cout<<dp[d]<<endl;
+    return 0;
+}
