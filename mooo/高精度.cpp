7386f1a0e52e9532b0a929e0b8b4fa6addf57e9c
os <os@osdeMacBook-Pro.local>
2017-10-30 10:22:23 +0800

    judged questionmooo/高精度

--- mooo/高精度.cpp
+++ mooo/高精度.cpp
@@ -29,6 +29,7 @@ int main(){
     cout<<d.data.to_string()<<endl;
     cout<<d.data[1]<<endl;
     cout<<c.data[1]<<endl;
+    cout<<(d.data[1] xor c.daya[1])<<endl;
     cout<<((c.data xor b.data)).to_string()<<endl;
     //cout<<(a+b).data.to_string()<<endl;
     return 0;
