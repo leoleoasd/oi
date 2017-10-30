a70c0a715f1ceb1969a081658127b682d679aa06
os <os@osdeMacBook-Pro.local>
2017-10-30 18:27:05 +0800

    judged question124/p239

---
+++ 124/p239.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+char tmp;
+#define MAXN 10000
+int data[MAXN]={0};
+
+int main(){
+    int i = 0;
+    while(cin>>tmp){
+        if(tmp!='#')
+            data[i]=1;
+        ++i
+    }
+    for(int j=0;j<i;++j){
+        if(data[j]!=0){
+            if(data[j*2+1]==data[j*2+2]){
+
+            }else{
+                cout<<"No"<<endl;
+                return 0;
+            }
+        }
+    }
+    cout<<"Yes"<<endl;
+    return 0;
+}
