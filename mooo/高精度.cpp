6bdc7f9152acdb84a6310040caa8eebcd7950c50
os <os@osdeMacBook-Pro.local>
2017-10-30 10:25:29 +0800

    judged questionmooo/高精度

--- mooo/高精度.cpp
+++ mooo/高精度.cpp
@@ -12,6 +12,9 @@ struct Bint{
     Bint(int a){data = bitset<size>(a);}
     Bint(unsigned long long a){data = bitset<size>(a);}
     Bint operator+(Bint a){
+        cout<<"正在把"<<data.to_string()<<endl;
+        cout<<"   和"<<a.data.to_string()<<endl;
+        cout<"相加"
         if((data & a.data).any()){
              return Bint(data xor a.data);
         }
@@ -21,17 +24,7 @@ struct Bint{
 int main(){
     Bint a(2);
     Bint b(3);
-    //cout<<(a).data.to_string()<<endl;
-    //cout<<(b).data.to_string()<<endl;
-    Bint c(((a.data & b.data)<<1));
-    Bint d(a.data xor b.data);
-    cout<<c.data.to_string()<<endl;
-    cout<<d.data.to_string()<<endl;
-    cout<<d.data[1]<<endl;
-    cout<<c.data[1]<<endl;
-    cout<<(d.data ^ c.data).to_string()<<endl;
-    cout<<(a+b).data.to_string()<<endl;;
-    cout<<((c.data xor d.data)).to_string()<<endl;
+    cout<<(a+b).data.to_string()<<endl;
     //cout<<(a+b).data.to_string()<<endl;
     return 0;
 }
