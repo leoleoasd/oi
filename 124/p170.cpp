48a9c4242f867e8af3387936c8e51fd158370056
os <os@osdeMacBook-Pro.local>
2017-10-31 19:47:11 +0800

    judged question124/p170

--- 124/p170.cpp
+++ 124/p170.cpp
@@ -3,8 +3,12 @@ using namespace std;
 int main(){
     int n,p;
     cin>>n;
-    for(int i=1;i<=n;++i){
-        if(n%i==0)cout<<i<<" ";
+    for(int i=2;i<n;++i){
+        if(n%i==0){
+            cout<<"no";
+            return 0;
+        }
     }
+    cout<<"yes";
     return 0;
 }
