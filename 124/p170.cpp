cdd281330aedd9f3619cac825a7f77377628dc0e
os <os@osdeMacBook-Pro.local>
2017-10-31 19:52:06 +0800

    judged question124/p170

--- 124/p170.cpp
+++ 124/p170.cpp
@@ -3,14 +3,10 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    int tmp;
-    int data[2]={0};
     for(int i=1;i<=n;++i){
-        cin>>tmp;
-        data[tmp]++;
+        for(int j=1;j<=i;++j)
+        cout<<"*";
+        cout<<endl;
     }
-    if(data[0]==data[1]) cout<<"invalid";
-    if(data[0]<data[1]) cout<<"X win";
-    if(data[0]>data[1]) cout<<"T win";
     return 0;
 }
