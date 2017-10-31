64e120a4d7f11f5972ae5ed015e1f390e50e5834
os <os@osdeMacBook-Pro.local>
2017-10-31 19:54:11 +0800

    judged question124/p170

--- 124/p170.cpp
+++ 124/p170.cpp
@@ -3,10 +3,15 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    for(int i=1;i<=n;++i){
-        for(int j=1;j<=i;++j)
-        cout<<"*";
-        cout<<endl;
+    for(int i=2;i<=n;++i){
+        if(n%i==0){
+            bool flag = false;
+            for(int j=2;j<i;++j){
+                if(i%j==0) flag = true;
+            }
+            if(i==2) flag = false;
+            flag ? (0) : (cout<<i<<" ");
+        }
     }
     return 0;
 }
