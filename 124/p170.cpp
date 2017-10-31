becc1d90da32f65d4d3c677584504edd8d2285f5
os <os@osdeMacBook-Pro.local>
2017-10-31 19:43:04 +0800

    judged question124/p170

--- 124/p170.cpp
+++ 124/p170.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 int main(){
     int a,b;
-    cin>>a>>b;
+    cin>>b;
     int output =0;
     for(int i=1;i<=b;++i){
-        output += i*i
+        output += i*i;
     }
     cout<<output;
     return 0;
