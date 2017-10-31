900a928f0b4a1c7dac7444936a67b7fb62cf08ee
os <os@osdeMacBook-Pro.local>
2017-10-31 19:59:38 +0800

    judged question124/p170

--- 124/p170.cpp
+++ 124/p170.cpp
@@ -1,23 +1,11 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int n;
-    cin>>n;
-    int data[50]={0};
-    data[1]=1;
-    data[2]=1;
-    data[3]=1;
-    data[4]=1;
-    data[5]=2;
-    data[6]=2;
-    data[7]=2;
-    data[8]=2;
-    data[9]=2;
-    data[10]=4;
-    for(int i=11;i<=n;++i){
-        data[i]=data[i-1]+data[i-5]+data[i-10];
-
+    int n,k;
+    cin>>n>>k;
+    for(int i=0;i<n;++i){
+        k = (k+1)*2;
     }
-    cout<<data[n];
+    cout<<k;
     return 0;
 }
