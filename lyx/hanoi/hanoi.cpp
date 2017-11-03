bb6fda7625a84224c6a9b71ba5c2cc53e1c77e1e
os <os@osdeMacBook-Pro.local>
2017-11-03 09:07:03 +0800

    judged questionlyx/hanoi/hanoi

--- lyx/hanoi/hanoi.cpp
+++ lyx/hanoi/hanoi.cpp
@@ -18,31 +18,47 @@ void hanoi_n(stack<int>& source,stack<int>& target,stack<int>& spare,int n){
         source.pop();
         target.push(tmp);
         ++nowstep;
-        cout<<"Moving"<<tmp<<endl;
         if(nowstep >= k) return;
         hanoi_n(spare,target,source,n-1);
+    }
+}
+bool f = false;
+void hanoi_k(stack<int>& k_source,stack<int>& k_target,stack<int>& k_spare,int n){
+    if(f) return;
+    if(n>0){
+        hanoi_k(k_source,k_target,k_spare,n-1);
+        if(f) return;
+        tmp = k_source.top();
+        k_source.pop();
+        k_spare.push(tmp);
+        ++nowstep;
+        if(k_source == source and k_target == target and k_spare == spare){
+            cout<<nowstep;
+            f = true;
+            return;
+        }
+        hanoi_k(k_target,k_source,k_spare,n-1);
+        if(f) return;
+        tmp = k_spare.top();
+        k_spare.pop();
+        k_target.push(tmp);
+        ++nowstep;
+        if(k_source == source and k_target == target and k_spare == spare){
+            cout<<nowstep;
+            f = true;
+            return;
+        }
+        hanoi_k(k_source,k_target,k_spare,n-1);
     }
 }
 int main(){
     cin>>n>>k;
     for(int i=n;i>=1;--i){
         source.push(i);
+        k_source.push(i);
     }
     hanoi_n(source,target,spare,n);
-    while(!source.empty()){
-        cout<<source.top()<<" ";
-        source.pop();
-    }
-    cout<<endl;
-    while(!spare.empty()){
-        cout<<spare.top()<<" ";
-        spare.pop();
-    }
-    cout<<endl;
-    while(!target.empty()){
-        cout<<target.top()<<" ";
-        target.pop();
-    }
-    cout<<endl;
+    nowstep = 0;
+    hanoi_k(k_source,k_target,k_spare,n);
     return 0;
 }
