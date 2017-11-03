84b4a3648f8acb347bd4e0d1673abbd80292b516
os <os@osdeMacBook-Pro.local>
2017-11-03 08:52:15 +0800

    judged questionlyx/hanoi/hanoi

--- lyx/hanoi/hanoi.cpp
+++ lyx/hanoi/hanoi.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
 #include <stack>
 using namespace std;
-stack<int> source;
-stack<int> target;
-stack<int> spare;
+stack<int> s[3];
 stack<int> k_source;
 stack<int> k_target;
 stack<int> k_spare;
 int n,k;
 int nowstep=0;
 int tmp;
-void hanoi_n(stack<int>& source,stack<int>& target,stack<int>& spare,int n){
+void hanoi_n(int source,int spare,int target,int n){
     if(n>0 and nowstep <k){
         hanoi_n(source,spare,target,n-1);
         if(nowstep > k) return;
-        tmp = source.top();
-        source.pop();
-        target.push(tmp);
+        tmp = s[source].top();
+        s[source].pop();
+        s[target].push(tmp);
         ++nowstep;
         cout<<"Moving"<<tmp<<endl;
         if(nowstep > k) return;
@@ -26,22 +24,22 @@ void hanoi_n(stack<int>& source,stack<int>& target,stack<int>& spare,int n){
 int main(){
     cin>>n>>k;
     for(int i=1;i<=n;++i){
-        source.push(i);
+        s[0].push(i);
     }
-    hanoi_n(source,target,spare,n);
-    while(!source.empty()){
-        cout<<source.top()<<" ";
-        source.pop();
-    }
-    cout<<endl;
-    while(!spare.empty()){
-        cout<<spare.top()<<" ";
-        spare.pop();
+    hanoi_n(0,1,2,n);
+    while(!s[0].empty()){
+        cout<<s[0].top()<<" ";
+        s[0].pop();
     }
     cout<<endl;
-    while(!target.empty()){
-        cout<<target.top()<<" ";
-        target.pop();
+    while(!s[1].empty()){
+        cout<<s[1].top()<<" ";
+        s[1].pop();
+    }
+    cout<<endl;
+    while(!s[2].empty()){
+        cout<<s[2].top()<<" ";
+        s[2].pop();
     }
     cout<<endl;
     return 0;
