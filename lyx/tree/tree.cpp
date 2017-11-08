8224dde45fb1a95d55e810f77cce71138cbfad71
leo <luyuxuanleo@gmail.com>
2017-11-08 10:49:53 +0800

    judged questionlyx/tree/tree

--- lyx/tree/tree.cpp
+++ lyx/tree/tree.cpp
@@ -16,8 +16,7 @@ int main(){
             m = max(w,m);
             sum += w;
         }
-        cout<<sum<<" "<<m;
-        cout<<(sum+(n*(n-1)/2 - (n-1) )*m  )<<endl;;
+        cout<<(sum+(n*(n-1)/2 - (n-1) )*(m+1)  )<<endl;;
     }
     return 0;
 }
