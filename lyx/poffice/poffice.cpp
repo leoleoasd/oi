4edb34fd6b3f8961d49d2a4996eece08fe4ce3c2
leo <luyuxuanleo@gmail.com>
2017-11-08 10:46:48 +0800

    judged questionlyx/poffice/poffice

--- lyx/poffice/poffice.cpp
+++ lyx/poffice/poffice.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using std::cin;
 using std::cout;
+using std::endl;
 int main(){
     return 0;
 }
--- lyx/tree/tree.cpp
+++ lyx/tree/tree.cpp
@@ -3,5 +3,18 @@ using std::cin;
 using std::cout;
 using std::endl;
 int main(){
+    int t,u,v,w,sum,m;
+    cin>>t;
+    while(--t){
+        cin>>n;
+        m = -1;
+        sum = 0;
+        for(int i=0;i<n-1;++i){
+            cin>>u>>v>>w;
+            m = max(w,m);
+            sum += w;
+        }
+        cout<<(sum+(n*(n-1)/2 - (n-1) )*max  )<<endl;;
+    }
     return 0;
 }
