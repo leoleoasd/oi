47c5cf82aa2584a638a08c428e5d43e0d1bd8cc6
leo <luyuxuanleo@gmail.com>
2017-11-08 09:00:43 +0800

    judged questionlyx/candy/candy

--- lyx/candy/candy.cpp
+++ lyx/candy/candy.cpp
@@ -5,18 +5,40 @@ using std::endl;
 
 #define MAXN 100000
 
-int t,n,m,y;
+int t,n=0,m,y;
 int x[MAXN]={0};
 int k[MAXN]={0};
-
+int sum;
 void solve(){
+    double fff;
+    fff = y/m;
+    for(int i=0;i<n;++i){
+        k[i] = x[i]*fff;
+    }
+    for(int i=0;i<n;++i){
+        cout<<k[i]<<" ";
 
+    }
+    cout<<endl;
+}
+
+void init(){
+    for(int i=0;i<n;++i){
+        x[i]=0;
+        k[i]=0;
+    }
+    sum=0;
 }
 
 int main(){
     cin>>t;
     while(t--){
-        cout<<t;
+        init();
+        cin>>n>>m>>y;
+        for(int i=0;i<n;++i){
+            cin>>x[i];
+        }
+        solve();
     }
     return 0;
 }
