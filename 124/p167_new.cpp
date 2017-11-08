2504b1c9ede88078880d5a6a5a7b753397630d12
leo <luyuxuanleo@gmail.com>
2017-11-08 22:11:01 +0800

    judged question124/p167_new

--- 124/p167_new.cpp
+++ 124/p167_new.cpp
@@ -11,7 +11,7 @@ int main(){
     }
     for(int i=0;i<n;++i){
         for(int j=0;j<n;++j){
-            if(dp[j]>data[i]){
+            if(dp[j]<data[i]){
                 dp[j]=data[i];
             }
         }
