dbf563cf6dba588b68019aee6c28092b8f5a1738
leo <luyuxuanleo@gmail.com>
2017-11-08 09:02:36 +0800

    judged questionlyx/candy/candy

--- lyx/candy/candy.cpp
+++ lyx/candy/candy.cpp
@@ -9,15 +9,20 @@ int t,n=0,m,y;
 int x[MAXN]={0};
 int k[MAXN]={0};
 int sum;
+int dsum;
 void solve(){
     double fff;
     fff = (double)m / y;
     for(int i=0;i<n;++i){
         k[i] = x[i] * fff;
+        dsum += k[i];
+    }
+    for(int i=0;dsum != sum;++i){
+        ++dsum;
+        ++k[i];
     }
     for(int i=0;i<n;++i){
         cout<<k[i]<<" ";
-
     }
     cout<<endl;
 }
@@ -28,6 +33,7 @@ void init(){
         k[i]=0;
     }
     sum=0;
+    dsum=0;
 }
 
 int main(){
