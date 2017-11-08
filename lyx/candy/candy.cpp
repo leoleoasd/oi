99bbae5133134ab60ec84dfa07eb832d5a5f65a6
leo <luyuxuanleo@gmail.com>
2017-11-08 08:27:49 +0800

    judged questionlyx/candy/candy

---
+++ lyx/candy/candy.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define MAXN 100000
+
+int t,n,m,y;
+int x[MAXN]={0};
+int k[MAXN]={0};
+
+void solve(){
+
+}
+
+int main(){
+    cin>>t;
+    while(t--){
+        cout<<t;
+    }
+    return 0;
+}
