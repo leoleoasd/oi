4f8845caf2905ee4982764c1c8d422d089355f8b
leo <luyuxuanleo@gmail.com>
2017-11-08 09:06:40 +0800

    judged questionlyx/candy/candy

--- lyx/candy/candy.cpp
+++ lyx/candy/candy.cpp
@@ -1,4 +1,5 @@
-#include<iostream>
+#include <iostream>
+#include <algorithm>
 using std::cin;
 using std::cout;
 using std::endl;
@@ -7,22 +8,32 @@ using std::endl;
 
 int t,n=0,m,y;
 int x[MAXN]={0};
-int k[MAXN]={0};
 int sum;
 int dsum;
+struct p{
+    int id;
+    int data;
+};
+bool cmp1(p a,p b){
+    return a.data > b.data;
+}
+bool cmp2(p a,p b){
+    return a.id > b.id;
+}
+struct p k[MAXN];
 void solve(){
     double fff;
     fff = (double)m / y;
     for(int i=0;i<n;++i){
-        k[i] = x[i] * fff;
-        dsum += k[i];
+        k[i].data = x[i] * fff;
+        dsum += k[i].data;
     }
     for(int i=0;dsum != m and i < n;++i){
         ++dsum;
-        ++k[i];
+        ++k[i].data;
     }
     for(int i=0;i<n;++i){
-        cout<<k[i]<<" ";
+        cout<<k[i].data<<" ";
     }
     cout<<endl;
 }
@@ -30,13 +41,19 @@ void solve(){
 void init(){
     for(int i=0;i<n;++i){
         x[i]=0;
-        k[i]=0;
+        k[i].id=i;
+        k[i].data=0;
     }
     sum=0;
     dsum=0;
 }
 
 int main(){
+    p[0].id=0;
+    p[1].id=2;
+    p[2].id=1;
+    sort(p,p+3,cmp2);
+    cout<<p[0].id;
     cin>>t;
     while(t--){
         init();
