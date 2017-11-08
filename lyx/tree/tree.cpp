24d98003e34a0cf0d13668e28aeecd8a12ca6770
leo <luyuxuanleo@gmail.com>
2017-11-08 11:13:11 +0800

    judged questionlyx/tree/tree

--- lyx/tree/tree.cpp
+++ lyx/tree/tree.cpp
@@ -11,9 +11,14 @@ int t,u,v,w,sum,m;
 int n;
 bitset<MAXN> vis;
 void dfs(int from,int to,int & ret){
+    v[from]=1;
     if(from==to)return;
     for(int i=1;i<=n;++i){
-
+        if(!v[i] and data[from][i]!=0){
+            dfs(i,to,ret);
+            ret = max(ret,data[from][i]);
+            if(i==to) return;
+        }
     }
 }
 int findmax(int from,int to){
@@ -29,14 +34,11 @@ int main(){
         for(int i=1;i<=n;++i)
             for(int j=1;j<=n;++j)
                 data[i][j]=0;
-
-        m = -1;
-        sum = 0;
         for(int i=0;i<n-1;++i){
             cin>>u>>v>>w;
-            m = max(w,m);
-            sum += w;
+            data[u][v]=w;
         }
+        cout<<finsmax(1,3);
         cout<<(sum+(n*(n-1)/2 - (n-1) )*(m+1)  )<<endl;;
     }
     return 0;
