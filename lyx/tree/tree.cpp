506422f0aa14fd7d5c75a09814a7d3228f86275b
leo <luyuxuanleo@gmail.com>
2017-11-08 10:59:41 +0800

    judged questionlyx/tree/tree

--- lyx/tree/tree.cpp
+++ lyx/tree/tree.cpp
@@ -1,14 +1,32 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using std::cin;
 using std::cout;
 using std::endl;
 using std::max;
+using std::bitset;
+int data[MAXN][MAXN]={0};
+int t,u,v,w,sum,m;
+int n;
+
+void dfs(int from,int to,int & ret){
+    if(from==to)return;
+    for(int i=1;i<=n;++i){
+
+    }
+}
+int findmax(int from,int to){
+    int ret =  -1;
+    dfs(from,to,ret);
+    return ret;
+}
 int main(){
-    int t,u,v,w,sum,m;
-    int n;
     cin>>t;
     while(t--){
         cin>>n;
+        for(int i=1;i<==n;++i)
+            for(int j=1;j<=n;++j)
+                data[i][j]=0;
+
         m = -1;
         sum = 0;
         for(int i=0;i<n-1;++i){
