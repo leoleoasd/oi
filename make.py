765a2729b1b4c2bcfa7bc0a128e9505cd7370b6f
leo <luyuxuanleo@gmail.com>
2017-11-09 10:53:21 +0800

    judged questionlyx/brazil/brazil

--- make.py
+++ make.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 import random
-with open('china.in', 'w') as f:
-    s=2000000000
-    n=500000
-    f.write(str(s)+" "+str(n)+"\n")
-    now = 0
-    for i in range(0,n):
-        step = random.randint(0,100)
-        pos = random.randint(1,5)
-
-        f.write(str(now+pos)+" "+str(now+pos+step)+" "+str(random.randint(1,5))+"\n")
-        now = now+pos+step
+with open('brazil.in', 'w') as f:
+    n=300
+    m=100
+    f.write("0 0 "+str(n)+" "+str(m)+"\n")
+    for i in range(0,n+m):
+        f.write(str(random.randint(1,100))+" "+str(random.randint(1,100))+"\n")
