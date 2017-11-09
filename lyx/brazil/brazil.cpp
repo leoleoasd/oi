e2d43c7ab9a6673d0fd9d772e1e141bed33e2d43
leo <luyuxuanleo@gmail.com>
2017-11-09 10:33:55 +0800

    judged questionlyx/brazil/brazil

--- lyx/brazil/brazil.cpp
+++ lyx/brazil/brazil.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <fstream>
+#include <stdint>
+#include <cmath>
 using namespace std;
 bool flag(int x1,int y1,int x2,int y2,int x3,int y3){
     if(x1 == x2 and x2==x3){
@@ -6,9 +9,41 @@ bool flag(int x1,int y1,int x2,int y2,int x3,int y3){
     }
     return ((double)(x2-x1)/(y2-y1))==((double)(x3-x2)/(y3-y2));
 }
+double dist(int x1,int y1,int x2,int y2){
+    return sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+}
+struct p{
+    int x;
+    int y;
+}goodman[305],badman[110];
+int x0,y0,n,m;
+#define MAXN 305
+int data[305][305]={0};
 int main(){
     ios::sync_with_stdio(false);
-    cout<<flag(0,0,1,1,2,2);
-    cout<<flag(0,0,1,2,2,2);
+    cin>>x0>y0>>n>>m;
+    int a,b;
+    for(int i=1;i<=n;++i){
+        cin>>a>>b;
+        goodman[i].x=a;
+        goodman[i].y=b;
+        for(int j=1;j<i;++j){
+            data[j][i]=data[i][j]=dist(a,b,goodman[j].x,goodman[j].y);
+        }
+        data[0][i]=data[i][0]=dist(a,b,x0,y0)*2;
+    }
+    for(int i=1;i<=m;++i){
+        cin>>a>>b;
+        for(int i=0;i<=n;++i){
+            for(int j=i;i<=n;++j){
+                if(data[i][j]){
+                    if(flag(goodman[i].x,goodman[i].y,goodman[i].x,goodman[i].y,a,b)){
+                        data[i][j]=data[j][i]=0;
+                    }
+                }
+            }
+        }
+    }
+    cout<<data[0][1];
     return 0;
 }
