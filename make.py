ee945ebf50a0c74928cbc634fed353d45a774061
leo <luyuxuanleo@gmail.com>
2017-11-09 11:14:53 +0800

    judged questionlyx/england/england

--- make.py
+++ make.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 import random
-with open('brazil.in', 'w') as f:
+with open('england.in', 'w') as f:
     m=100000
     f.write(str(m)+"\n")
     for i in range(0,m):
