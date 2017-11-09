6071c647bfd303614b623ddd9e464251e78a6037
leo <luyuxuanleo@gmail.com>
2017-11-09 11:04:15 +0800

    judged questionlyx/england/england

--- lyx/england/england.cpp
+++ lyx/england/england.cpp
@@ -2,5 +2,35 @@
 using namespace std;
 int main(){
     ios::sync_with_stdio(false);
+    int n;
+    int data[100020]={0};
+    cin>>n;
+    for(int i=0;i<n;++i){
+        cin>>data[i];
+    }
+    int output = -1;
+    int f;
+    int lo;
+    int l = 0;
+    for(int i=51;i>=0;--i){
+        lo = 0;
+        f = 1;
+        l = 0;
+        for(int j=0;j<n;++j){
+            if(data[j]>i){
+                if(f==1){
+                    ++lo;
+                }else{
+                    f=1;
+                    lo=1;
+                }
+            }else{
+                f=0;
+                l = max(lo,l);
+            }
+        }
+        cout<<l;
+        return 0;
+    }
     return 0;
 }
