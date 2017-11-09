d457d0554c1de223565925dc184729b6247059c3
leo <luyuxuanleo@gmail.com>
2017-11-09 10:23:47 +0800

    judged questionlyx/brazil/brazil

--- lyx/brazil/brazil.cpp
+++ lyx/brazil/brazil.cpp
@@ -4,7 +4,7 @@ bool flag(int x1,int y1,int x2,int y2,int x3,int y3){
     if(x1 == x2 and x2==x3){
         return true;
     }
-    return ((double)(x2-x1)/(y2-y1))==((double)(x3-x2)/(y3-y2))
+    return ((double)(x2-x1)/(y2-y1))==((double)(x3-x2)/(y3-y2));
 }
 int main(){
     ios::sync_with_stdio(false);
