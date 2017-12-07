10ac2e942e4a4cd49cd4ec8f58ceee5e0b959258
leo <luyuxuanleo@gmail.com>
2017-12-07 17:23:42 +0800

    judged questionlg/1259

---
+++ lg/1259.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int gcd(int a,b){
+    if(a<b){
+        swal(a,b);
+    }
+    if(a % b == 0){
+        return b;
+    }
+    return gcd(b,a%b);
+}
+int main(){
+    cout<<gcd(20,15);
+    return 0;
+}
