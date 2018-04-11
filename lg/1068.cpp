b99b50edf87515e8f1c909af681efd758332cabb
leo <luyuxuanleo@gmail.com>
2018-04-11 14:07:08 +0800

    judged questionlg/1068

--- lg/1068.cpp
+++ lg/1068.cpp
@@ -9,16 +9,30 @@ class Player{
             this->score=0;
             this->id=0;
         }
-
+        Player(int a,int b){
+            this->id=a;
+            this->score=b;
+        }
         bool operator<(const Player &b){
             return this->score < b.score;
         }
+
+        bool operator>(const Player &b){
+            return this->score > b.score;
+        }
 };
 
+priority_queue<PLayer> que;
+
 int main(){
-    Player a,b;
-    a.score=1;
-    b.score=2;
-    cout<<(a<b);
+    int m,n;
+    int t1,t2;
+    cin>>n>>m;
+    for(int i=0;i<n;++i){
+        cin>>t1>>t2;
+        Player temp(t1,t2);
+        que.push(temp);
+    }
+    cout<<que.top().id;
     return 0;
 }
