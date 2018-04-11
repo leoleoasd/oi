332e2bf46ae5cefa6e1a7d856c69d55c5012b755
leo <luyuxuanleo@gmail.com>
2018-04-11 14:15:12 +0800

    judged questionlg/1068

--- lg/1068.cpp
+++ lg/1068.cpp
@@ -13,10 +13,10 @@ class Player{
             this->id=a;
             this->score=b;
         }
-        bool const operator<(Player &b){
+        bool operator<(Player &b) const{
             return this->score < b.score;
         }
-        bool operator>(Player &b){
+        bool operator>(Player &b) const{
             return this->score > b.score;
         }
 };
