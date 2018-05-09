b17cd4075384c5eee593e204bd92ab019f0b045b
Leo Lu <luyuxuanleo@gmail.com>
2018-05-09 11:10:53 +0800

    judged questionlg/1051

--- lg/1051.cpp
+++ lg/1051.cpp
@@ -1,10 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define  MAXN 105
+
 class student {
     public:
         string name;
         int money;
         int id;
+        Student(){
+            money=0;
+            id=0;
+            name="";
+        }
         bool operator<(const student B) const{
             if(money != B.money){
                 return money < B.money;
@@ -18,13 +25,36 @@ class student {
             return id < B.id;
         }
 };
-student data[20];
+student data[MAXN];
 int main(){
-    data[0].money=20;
-    data[1].money=20;
-    data[0].id=0;
-    data[1].id=1;
-    sort(data,data+2,greater<student>());
-    cout<<data[0].money<<" "<<data[0].id;
+    int n;
+    int qmcj,bjcj,lws;
+    char xsgb,xbsf;
+    cin>>n;
+    for(int i=0;i<n;++i){
+        cin>>data[i].name;
+        data[i].id=i;
+        cin>>qmcj>>bjcj>>xsgb>>xbsf>>lws;
+        if(qmcj > 80 and lws >= 1){
+            data[i].money+=8000;
+        }
+        if(qmcj > 85 and bjcj > 80){
+            data[i].money += 4000;
+        }
+        if(qmcj > 90){
+            data[i].money += 2000;
+        }
+        if(qmcj > 85 and xbsf == "Y"){
+            data[i].money += 1000;
+        }
+        if(bjcj > 80 and xsgb == "Y"){
+            data[i].money += 850;
+        }
+    }
+    sort(data,data+n.greater<student>());
+    cout<<data[0].name<<endl<<data[0].money<<endl;
+    int sum == 0;
+    for(int i=0;i<n;++i;)sum += data[i].money;
+    cout<<sum;
     return 0;
 }
