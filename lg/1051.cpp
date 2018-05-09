e243c8ed74b9802e094573c3e133913ed57e129a
Leo Lu <luyuxuanleo@gmail.com>
2018-05-09 10:31:59 +0800

    judged questionlg/1051

--- lg/1051.cpp
+++ lg/1051.cpp
@@ -24,7 +24,7 @@ int main(){
     data[1].money=30;
     data[0].id=0;
     data[1].id=1;
-    sort(data,data+2);
+    sort(data,data+2,greater<student>);
     cout<<data[0].money<<" "<<data[0].id;
     return 0;
 }
