c7a0cf9e81a11ef6ea03caa7c94b5ef93da6f7ac
Leo Lu <luyuxuanleo@gmail.com>
2018-05-11 07:58:49 +0800

    judged questionlg/1603

---
+++ lg/1603.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> data;
+
+void add(string &x){
+    if(x == "one")data.push_back(01);
+    else if(x == "two")data.push_back(04);
+    else if(x == "three")data.push_back(09);
+    else if(x == "four")data.push_back(16);
+    else if(x == "five")data.push_back(25);
+    else if(x == "six")data.push_back(36);
+    else if(x == "seven")data.push_back(49);
+    else if(x == "eight")data.push_back(64);
+    else if(x == "nine")data.push_back(81);
+    else if(x == "ten")data.push_back(00);
+    else if(x == "eleven")data.push_back(21);
+    else if(x == "twelve")data.push_back(44);
+    else if(x == "thirteen")data.push_back(69);
+    else if(x == "fourteen")data.push_back(96);
+    else if(x == "fifteen")data.push_back(25);
+    else if(x == "sixteen")data.push_back(56);
+    else if(x == "seventeen")data.push_back(89);
+    else if(x == "eighteen")data.push_back(24);
+    else if(x == "nineteen")data.push_back(61);
+    else if(x == "twenty")data.push_back(0);
+    else if(x == "a")data.push_back(01);
+    else if(x == "both")data.push_back(04);
+    else if(x == "another")data.push_back(09);
+    else if(x == "first")data.push_back(01);
+    else if(x == "second")data.push_back(04);
+    else if(x == "third")data.push_back(09);
+}
+int main(){
+    string tmp;
+    for(int i=0;i<6;++i){
+        cin>>tmp;
+        add(tmp);
+    }
+    sort(data.begin(),data.end());
+    cout<<data[0];
+    return 0;
+}
