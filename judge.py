2d55ab2c9ef6c2b19f5b2a60433eaa094ab8b222
Leo Lu <luyuxuanleo@gmail.com>
2018-09-07 18:40:34 +0800

    judged questionlg/1538

--- judge.py
+++ judge.py
@@ -3,6 +3,7 @@
 import sys
 from os import system
 from os import chroot
+import time
 byxx = "  "
 def main():
     t = sys.argv[1]
@@ -11,9 +12,12 @@ def main():
     system("g++-8 -g  -o "+t+" "+t+".cpp"+byxx)
     print("Compile Endded")
     print("------------------------------------------------------------------------")
+    before = time.time()
     system("./"+t)
+    after = time.time()
     print("")
     print("------------------------------------------------------------------------")
+    print("Time: {}".format(after-before))
     print("git committing")
     system("git add -A")
     system("git commit -a -m \"judged question"+t+"\" 1>/dev/null 2>&1")
